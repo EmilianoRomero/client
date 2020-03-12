13199f9567329fc0b317c8b976ad3cf52112c923
trance-cyberian <trance.cyberian@gmail.com>
2020-03-12 20:46:14 +0100

    new style basics plus done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import Landing from "./screen/Landing";
 import Cities from "./cities";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
-import { Container } from 'react-bootstrap';
-import { Row } from 'react-bootstrap';
-//import { Col } from 'react-bootstrap';
 import Logo from "./component/00Images/MYtineraryLogo.png";
 import BrowserButton from "./component/Browsing/BrowserButton";
-import LoginButton from "./component/Login/LoginButton";
-import RegisterButton from "./component/Register/RegisterButton";
+import LoginLink from "./component/Login/LoginLink";
+import RegisterLink from "./component/Register/RegisterLink";
 import HomeButton from "./component/Home/HomeButton";
 
 export default class App extends Component {
@@ -17,40 +14,35 @@ export default class App extends Component {
     return (
       <BrowserRouter>
         <div className="App">
-          <Container>
             <Switch>
               <Route exact path="./screen/Landing" component={Landing} />
               <Route exact path="./cities" component={Cities} />
             </Switch>
-            <Row>
+            <div className="app-container">
+              <div className="logo">
               <img className="logoImg" src={Logo} alt="" />
-            </Row>
-            <Row>
-              <h2 className="motoTxt">
+              </div>
+            <div className="moto">
+              <h1 className="motoTxt">
                 Find your perfect trip, designed by insiders who know and love
                 their cities
-              </h2>
-            </Row>
-            <Row>
+              </h1>
+              </div>
+            <div className= "start">
               <h1 className="startTxt">Start browsing</h1>
-            </Row>
-            <Row>
+            </div>
               <BrowserButton />
-            </Row>
-            <Row>
-              <h2 className="ownItineraryTxt">
+            <div className="legend">
+              <h2 className="legendTxt">
                 Want to build your own MYtinerary
               </h2>
-            </Row>
-            <Row>
-              <LoginButton />
-              <RegisterButton />
-            </Row>
-            <Row>
-              ssName="goHome">
+            </div>
+            <div class= "loginReg">
+              <LoginLink />
+              <RegisterLink />
+            </div>
               <HomeButton />
-            </Row>
-          </Container>
+          </div>
         </div>
       </BrowserRouter>
     );
--- src/component/Browsing/BrowserButton.js
+++ src/component/Browsing/BrowserButton.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import "./BrowserButton.css";
 import Arrow from "../00Images/circled-right-2.png";
+import { Link } from "react-router-dom";
+import "./BrowserButton.css";
 
-const BrowserButton = props => (
-  <div className="startBrowsing">
-    <img className="arrowImg" src={Arrow} alt="" />
-  </div>
-);
+export default class BrowserButton extends Component {
+  render() {
+    return (
+      <div className="startBrowsing">
+        <Link className="startBrowsing" exact to="/">
+          <img
+            src={Arrow}
+            className="arrowImg"
+            alt=""
+            activeClassName="browingLink-active"
+          />
+        </Link>
+      </div>
+    );
+  }
+}
 
-export default BrowserButton;
+// traditionally 
+{/* <a href="https://github.com/Joeyryanbridges">
+<Image src="giticon.png" className="githubIcon" />
+</a> */}
